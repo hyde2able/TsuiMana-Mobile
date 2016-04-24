be3ee65979310658ba27e0c9b79fa592fb703cc4
hyde2able <hyde31415@gmail.com>
2016-04-24 18:19:11 +0900

    .gitignore is now working

--- Pods/Target Support Files/Pods-TsuimanaMobile/Pods-TsuimanaMobile-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TsuimanaMobile : NSObject
-@end
-@implementation PodsDummy_Pods_TsuimanaMobile
-@end
--- Pods/Target Support Files/Pods-TsuimanaMobile/Pods-TsuimanaMobile-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_TsuimanaMobileVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TsuimanaMobileVersionString[];
-
