a8c85a54af0bb02d603fb4b65072353f0dd7c698
hyde2able <hyde31415@gmail.com>
2016-04-24 19:23:42 +0900

    youtubeを見れるようになったけどuser-agentの関係でvevoとかが見れない

--- TsuimanaMobile/EvideoViewController.swift
+++ TsuimanaMobile/EvideoViewController.swift
@@ -17,10 +17,12 @@ class EvideoViewController: UIViewController {
     var timedtexts = [Timedtext]()
     private var isLoading = false
     private var evideo: Evideo? = nil
+    @IBOutlet private weak var ytPlayer: UIWebView!
     
     override func viewDidLoad() {
         evideo = self.contextValue()
         fetchData(true)
+        ytPlayerInit()
         print(evideo)
     }
     
@@ -46,5 +48,36 @@ class EvideoViewController: UIViewController {
             }
         }
     }
-        
+    
+    func webViewDidFinishLoad(webView: UIWebView!) {
+        print("webViewDidFinishLoad")
+        print(ytPlayer.request?.URL)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView!) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        print("webViewDidStartLoad")
+        print(ytPlayer.request?.URL)
+    }
+    
+    
+    
+    private func ytPlayerInit() {
+        guard let evideo = self.evideo, videoId = evideo.videoId else { return }
+        self.changeUserAgent()
+        let html: String! = "<div style='position:relative;width:100%;padding:56.25% 0 0 0'><iframe src='http://www.youtube.com/embed/\(videoId)' frameborder='0' style='position:absolute;top:0;left:0;width:100%;height:100%'></div>"
+        self.ytPlayer.loadHTMLString(html, baseURL: nil)
+        self.ytPlayer.scrollView.bounces = false
+        self.ytPlayer.allowsInlineMediaPlayback = true
+    }
+    
+    private func changeUserAgent(){
+        let userAgent: String! = self.ytPlayer.stringByEvaluatingJavaScriptFromString("navigator.userAgant")
+        let userAgentStr = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"
+        let customUserAgent: String = userAgent.stringByAppendingString(userAgentStr)
+        let dic: NSDictionary = ["UserAgent": customUserAgent]
+        NSUserDefaults.standardUserDefaults().registerDefaults(dic as! [String : AnyObject])
+    }
+    
 }
